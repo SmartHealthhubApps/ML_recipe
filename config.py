84c1ffe2d6152be60c9d0a28851da97cf61f7a3c
Aleksey Komissarenko <lepyoshik@gmail.com>
2024-04-21 19:32:36 +0300

    Changed device logic

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ import torch
 class CFG:
     img_path = 'dataset_pascal-voc/images'
     ann_path = 'dataset_pascal-voc/annotations'
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        device = torch.devcice('mps')
+    else:
+        device = torch.device('cpu')
 
     max_len = 300
     img_size = 640
